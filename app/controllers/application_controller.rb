292c249bf1701fce3d8267aec5c4dc8111f7e32a
Kevin Hughes <kevin.hughes@shopify.com>
2016-02-18 14:47:45 -0500

    moved to ShopifyApp

--- app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::Base
-  include ShopifyApp::Controller
-  protect_from_forgery
-end
--- config/initializers/shopify_session_repository.rb
+++
@@ -1,7 +0,0 @@
-if Rails.configuration.cache_classes
-  ShopifyApp::SessionRepository.storage = SessionStorage
-else
-  ActionDispatch::Reloader.to_prepare do
-    ShopifyApp::SessionRepository.storage = SessionStorage
-  end
-end
